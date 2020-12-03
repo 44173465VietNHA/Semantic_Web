8f9150a1cb43490bb2f8c30edc633ac6302474ee
44173465NHA <nguyenvietbn1999@gmail.com>
2020-12-03 22:04:27 +0700

    .

--- Tagging_CRF/posTagging.py
+++ Tagging_CRF/posTagging.py
@@ -12,7 +12,7 @@ def sent2postagging(sent):
  sent = ViPosTagger.postagging(doc)
  result = ''
  for i in range(len(sent[0])):
-  result = result + sent[0][i] + " " + sent[1][i] + "\n"
+  result = result + sent[0][i] + "\t" + sent[1][i] + "\t\n"
  return result
 
 def sent2postagging_formatArray(sent):
@@ -20,7 +20,7 @@ def sent2postagging_formatArray(sent):
  sent = ViPosTagger.postagging(doc)
  result = []
  for i in range(len(sent[0])):
-  result.append([sent[0][i],sent[1][i]])
+  result.append([sent[0][i],sent[1][i],""])
  return result
 
 # example = 'TRƯỜNG LÀ ĐẠI HỌC TOP ĐẦU CỦA CẢ NƯỚC, được nhiều trường đại học quốc tế công nhận chương trình đào tạo và thiết lập quan hệ đào tạo, trong đó có Đại học'
@@ -28,20 +28,20 @@ def sent2postagging_formatArray(sent):
 
 path = ""
 
-# for filename in os.listdir(dir):
-#  if filename.endswith(".txt"):
-#   path = os.path.join(dir, filename)
-#   f = open(path, 'r', encoding = 'utf-8')
-#   text = f.read()
-#   result = sent2postagging_formatArray(text)
-#   pd.DataFrame(result).to_csv('Tagging_CRF/historyRelic/' + filename.split('.')[0]+'.csv', header = False, index = False, encoding='utf-8')
-
 for filename in os.listdir(dir):
  if filename.endswith(".txt"):
   path = os.path.join(dir, filename)
   f = open(path, 'r', encoding = 'utf-8')
   text = f.read()
-  fresult = open('Tagging_CRF/historyRelic/'+filename, 'wb')
-  fresult.write(sent2postagging(text).encode('utf-8'))
-  fresult.close()
-  f.close()
+  result = sent2postagging_formatArray(text)
+  pd.DataFrame(result).to_csv('Tagging_CRF/historyRelic/' + filename.split('.')[0]+'.csv', header = False, index = False, encoding='utf-8')
+
+# for filename in os.listdir(dir):
+#  if filename.endswith(".txt"):
+#   path = os.path.join(dir, filename)
+#   f = open(path, 'r', encoding = 'utf-8')
+#   text = f.read()
+#   fresult = open('Tagging_CRF/historyRelic/'+filename, 'wb')
+#   fresult.write(sent2postagging(text).encode('utf-8'))
+#   fresult.close()
+#   f.close()
