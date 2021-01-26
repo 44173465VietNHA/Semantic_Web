a2d2d22fb6e5b916d90c47996bab673822bfaf6f
44173465NHA <nguyenvietbn1999@gmail.com>
2021-01-27 05:21:37 +0700

    Add source code colab

--- CrawlData/craw/craw/spiders/crawHistoryRelic2.py
+++ CrawlData/craw/craw/spiders/crawHistoryRelic2.py
@@ -1,6 +1,11 @@
 import scrapy
 from craw.items import CrawItem
 
+import logging, sys
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+import os
+import time
+
 # run in sematic_web/CrawlData/craw
 
 crawHistory2 = "crawHistory2"
@@ -14,18 +19,27 @@ class historySpider2(scrapy.Spider):
   for page in range(1,endPage):
    yield self.make_requests_from_url(historyUrl+'?start=%s' %page)
  def parse(self, response):
-  historyUrl = response.xpath('//*[@class="span12"]/div/div/h2/a/@href').extract()
+  historyUrl = response.xpath('//*[@itemprop="blogPost"]/a/@href').extract()
   print("length"+str(len(historyUrl)))
+  storeLink = []
+  f = open('link/link.txt','a')
+  f.truncate()
   for link in historyUrl:
+   print("link:" + 'https://dulichvinhphuc.gov.vn'+link)
+   f.write((link.strip()+'\n'))
+   storeLink.append(link)
+  f.close()
+  for link in storeLink:
    yield scrapy.Request('https://dulichvinhphuc.gov.vn'+link, callback=self.saveFile)
  def saveFile(self, response):
-  name = response.xpath('//*[@class="page-header"]/h2/a/text()').extract()
+  name = response.xpath('//*[@itemprop="name"]/a/text()').extract()
   title = response.xpath('string(//*[@class="articleBody"]/text())').extract()
   name = ''.join(name).strip()
   nameFile = name + '.txt'
+  logging.debug("saveFile"+nameFile)
   title = (''.join(title).strip().replace('\n','') + '\n').encode('utf-8')
   content = ''
-  contentXpathArrays = response.xpath('//*[@id="content"]/div[3]/div[2]/p[1]//text()').extract()
+  contentXpathArrays = response.xpath('//*[@itemprop="articleBody"]//text()').extract()
   for p in contentXpathArrays:
    content = content + ''.join(p)
   content = content.strip().encode('utf-8')
